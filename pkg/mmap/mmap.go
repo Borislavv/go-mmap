4fba4b7e2367f8e5bd04be2fdc5c07069bc69e4f
Boris <glazunov2142@icloud.com>
2024-06-24 11:36:45 +0300

    feat: mmap package.

---
+++ pkg/mmap/mmap.go
@@ -0,0 +1,64 @@
+package mmap
+
+import (
+	"sync"
+	"syscall"
+)
+
+// Read is take a path to file, number of readers and size of chunk.
+func Read(path string, readers int, chunkSize int64) (chunksCh chan []byte, closeFunc func(), err error) {
+	fd, err := syscall.Open(path, syscall.O_RDONLY, 0)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var stat syscall.Stat_t
+	if err = syscall.Fstat(fd, &stat); err != nil {
+		func() { _ = syscall.Close(fd) }()
+		return nil, nil, err
+	}
+
+	data, err := syscall.Mmap(fd, 0, int(stat.Size), syscall.PROT_READ, syscall.MAP_SHARED)
+	if err != nil {
+		func() { _ = syscall.Close(fd) }()
+		return nil, nil, err
+	}
+
+	closeFunc = func() {
+		_ = syscall.Close(fd)
+		_ = syscall.Munmap(data)
+	}
+
+	offsetsCh := make(chan int64, readers)
+	defer close(offsetsCh)
+
+	chunksCh = make(chan []byte, readers)
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < readers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for offset := range offsetsCh {
+				end := offset + chunkSize
+				if end > stat.Size {
+					end = stat.Size
+				}
+
+				chunksCh <- data[offset:end]
+			}
+		}()
+	}
+	defer func() {
+		wg.Wait()
+		close(chunksCh)
+	}()
+
+	for offset := int64(0); offset < stat.Size; offset += chunkSize {
+		offsetsCh <- offset
+	}
+	close(offsetsCh)
+
+	return chunksCh, closeFunc, nil
+}
